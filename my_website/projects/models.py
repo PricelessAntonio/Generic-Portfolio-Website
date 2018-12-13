51967851f4af908052856bcea2a44b605a8443dc
Antonio Jimenez <pricelessantonio@gmail.com>
2018-12-13 02:40:46 -0800

    Adding Projects models
    - Created Project model class
    - Created ProjectPost model class
    - Registered models on admin page through admin.py

--- my_website/projects/models.py
+++ my_website/projects/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Project(models.Model):
+    project_date = models.DateTimeField(auto_now_add=True)
+    project_title = models.CharField(max_length=100)
+    project_description = models.CharField(max_length=300)
+    project_completion_status = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+
+class ProjectPost(models.Model):
+    project_post_date = models.DateTimeField(auto_now_add=True)
+    project_post_title = models.CharField(max_length=100)
+    project_post_content = models.CharField(max_length=40000)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL, null=True)
