aa38357dac6351a0d16ea165cdcf8f18c5b4857d
Antonio Jimenez <pricelessantonio@gmail.com>
2018-12-15 01:03:59 -0800

    Removing ProjectImage model class from Projects app

--- my_website/projects/admin.py
+++ my_website/projects/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
-from .models import Project, ProjectPost, ProjectImage
+from .models import Project, ProjectPost
 
 admin.site.register(Project)
 admin.site.register(ProjectPost)
-admin.site.register(ProjectImage)
--- my_website/projects/models.py
+++ my_website/projects/models.py
@@ -16,14 +16,6 @@ class ProjectPost(models.Model):
     project_post_date = models.DateTimeField(auto_now_add=True)
     project_post_title = models.CharField(max_length=100)
     project_post_content = models.CharField(max_length=40000)
+    project_post_image = models.ImageField()
     project = models.ForeignKey(Project, on_delete=models.SET_NULL, blank=True,
                                 null=True)
-
-
-class ProjectImage(models.Model):
-    project_image_date = models.DateTimeField(auto_now_add=True)
-    project_image_title = models.CharField(max_length=100)
-    project_image_upload = models.ImageField()
-    project_image_description = models.CharField(max_length=300)
-    project_post = models.ForeignKey(ProjectPost, on_delete=models.SET_NULL,
-                                     blank=True, null=True)
